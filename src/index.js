a55de0964bc7d2f2d4bcb081a342d34092486fb1
juan salazar <juancamilosalazarrestrepo@gmail.com>
2021-09-14 17:45:05 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const { mongoose } = require("./database");
+
+const app = express();
+
+//Settings
+
+app.set("port", process.env.PORT || 3000);
+
+//Middlewares
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+//Routes
+
+app.use("/api/tasks", require("./routes/tasks.routes"));
+
+//Static files
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//starting  the server
+app.listen(app.get("port"), () => {
+  console.log(`Server on Port ${app.get("port")}`);
+});
